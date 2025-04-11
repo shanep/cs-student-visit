52f5ea4b3046857644bb0b79d66cf8018a3c66af
Shane K. Panter <shane@foundationcode.com>
2025-04-11 19:20:10 +0000

    Finished hello world

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,19 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String name = getName();
+        greet(name);
     }
 
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public static String getName() {
+        System.out.print("What is your name? ");
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        String name = scanner.nextLine();
+        scanner.close();
+        return name;
+    }
 }
